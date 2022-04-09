4c6cb610229b027a7a1482df069dbb015e1cad01
AkiKoide <akikoide924@gmail.com>
2022-04-09 12:57:31 +0200

    Added html

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-
+        <Weather />
         <footer>
           This project was created by Aki Koide and is{" "}
           <a
             href="https://github.com/AkiKoide/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on github
           </a>
