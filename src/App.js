24416157e894cbee8d16ee287f76cce42b429799
AkiKoide <akikoide924@gmail.com>
2022-04-09 11:46:03 +0200

    added boostrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was created by Aki Koide and is{" "}
-        <a href="https://github.com/AkiKoide/react-weather-app" target="_blank">
-          open-sourced on github
-        </a>
-      </footer>
+        <footer>
+          This project was created by Aki Koide and is{" "}
+          <a
+            href="https://github.com/AkiKoide/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
