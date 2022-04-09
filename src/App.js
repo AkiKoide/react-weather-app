b711e0f39a62824f4ec5f62e1c4d487aaacd3add
AkiKoide <akikoide924@gmail.com>
2022-04-09 11:28:57 +0200

    Added github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">weather</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        This project was created by Aki Koide and is{" "}
+        <a href="https://github.com/AkiKoide/react-weather-app" target="_blank">
+          open-sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
