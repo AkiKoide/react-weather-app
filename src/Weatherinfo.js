32209835a1f6e48914375194729ce6245d3d61c3
AkiKoide <akikoide924@gmail.com>
2022-04-10 11:34:29 +0200

    added icon

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,7 +14,7 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <img src={props.data.iconUrl} alt={props.data.description} />
+          <WeatherIcon code={props.data.icon} size={52} />
 
           <span className="temperature">
             {Math.round(props.data.temperature)}
@@ -22,7 +23,6 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 15%</li>
             <li>Humidity: {props.data.humidity} %</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
